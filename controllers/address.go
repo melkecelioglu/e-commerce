11c50eae6c3a805aee3ac1d1ecaba72ea858d06a
melkecelioglu <melkecelioglu@gmail.com>
2023-03-08 22:12:02 +0300

    set

---
+++ controllers/address.go
@@ -0,0 +1,34 @@
+package controllers
+import (
+
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+	"github.com/melkecelioglu/e-commerce/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+
+func AddAddress() gin.HandleFunc {
+
+
+}
+
+
+func EditHomeAddress() gin.HandlerFunc{
+
+}
+
+
+func EditWorkAddress() gin.HandlerFunc{
+
+}
+
+
+func DeleteAddress() gin.HandlerFunc{
+
+}
