925bb98bbe4d282a018e40a012645ef0a032a269
melkecelioglu <melkecelioglu@gmail.com>
2023-03-17 23:40:32 +0300

    [ADD]-controllers/cart

--- controllers/cart.go
+++ controllers/cart.go
@@ -35,8 +35,22 @@ func NewApplication(prodCollection *mongo.Collection, userCollection *mongo.Coll
 func (app *Application) AddToCart() gin.HandlerFunc{
 	return func(c *gin.Context){
 		productQueryID := c.Query("id")
-		if productQueryID == ""{
+		if productQueryID == "" {
 			log.Println("product ID is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("product id is empty"))
+			return 
 		}
+		userQueryID := c.Query("userID")
+		if userQueryID == ""{
+			log.Println("user id is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("user id is empty"))
+			return 
+		}
+		//productID, err := primitive.ObjectIDFromHex(productQueryID)
+		//if err != nil{
+		//	log.Println(err)
+		//	c.AbortWithStatus(http.StatusInternalServerError)
+		//	return
+		//}
 	}
 }
