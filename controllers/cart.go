b1c083d5c26108ec0576f15b031fa3142bed2f19
melkecelioglu <melkecelioglu@gmail.com>
2023-03-18 23:44:39 +0300

    [ADD]-controllers/cart

--- controllers/cart.go
+++ controllers/cart.go
@@ -46,11 +46,29 @@ func (app *Application) AddToCart() gin.HandlerFunc{
 			_ = c.AbortWithError(http.StatusBadRequest, errors.New("user id is empty"))
 			return 
 		}
-		//productID, err := primitive.ObjectIDFromHex(productQueryID)
-		//if err != nil{
-		//	log.Println(err)
-		//	c.AbortWithStatus(http.StatusInternalServerError)
-		//	return
-		//}
+		productID, err := primitive.ObjectIDFromHex(productQueryID)
+		if err != nil{
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		err = database.AddProductToCart(ctx, app.prodCollection, app.userCollection, productID, userQueryID)
+		if err != nil{
+			c.IndentedJSON(http.StatusInternalServerError, err)
+
+		}
+		c.IndentedJSON(200, "Successfully added to the cart")
+	}
+}
+
+
+func(app *Application) RemoveItem() gin.HandlerFunc{
+
+	return func (c *gin.Context){
+		productQueryID := c.
 	}
 }
